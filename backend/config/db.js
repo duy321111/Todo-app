6b52445c697c91a5124f06cc8d5d04ec47731de6
duy <duy321111@gmail.com>
2026-07-05 13:36:31 +0700

    Connect MongoDB with Mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,14 @@
-// Database configuration
-module.exports = {
-  // Add your database configuration here
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.error("Database connection failed:", error.message);
+        process.exit(1);
+    }
 };
+
+export default connectDB;
