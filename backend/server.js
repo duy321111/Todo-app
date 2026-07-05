7be14a13279b4f510ff66517e1e50f339a0f7c50
duy <duy321111@gmail.com>
2026-07-05 15:32:04 +0700

    Implement Todo CRUD API

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,25 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import todoRoutes from "./routes/todoRoutes.js";
+
 dotenv.config();
+
+//DB
 connectDB();
 const app = express();
 
+//cross-origin requests, parse JSON.
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Todo API is running...");
-});
+//Routes
+app.use("/api/todos", todoRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+// Start the API server.
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
