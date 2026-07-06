b1e64698b663dbe62a981ce9f08aad05a7149fa1
duy <duy321111@gmail.com>
2026-07-06 21:59:58 +0700

    fix CORS

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,18 @@ import todoRoutes from "./routes/todoRoutes.js";
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://127.0.0.1:3000",
+  "http://127.0.0.1:5173"
+].filter(Boolean);
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/todos", todoRoutes);
