6bec6aa1cd33f708e5653dd766e3b7d13434ff83
duy <duy321111@gmail.com>
2026-07-06 22:02:52 +0700

    rebase

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,7 @@ import todoRoutes from "./routes/todoRoutes.js";
 
 const app = express();
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "http://127.0.0.1:3000",
-  "http://127.0.0.1:5173"
-].filter(Boolean);
-
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/todos", todoRoutes);
